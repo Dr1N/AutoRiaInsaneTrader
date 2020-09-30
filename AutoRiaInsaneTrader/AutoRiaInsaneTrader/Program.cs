cf16fe7cdefba9edf1945d9245edaa0622025fff
Dr1N <drn.exp@gmail.com>
2020-09-30 14:56:39 +0300

    initial commit

---
+++ AutoRiaInsaneTrader/AutoRiaInsaneTrader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoRiaInsaneTrader
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
